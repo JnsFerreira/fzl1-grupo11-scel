b860ccaf87043350d2ef3d685922a9b1fc0b7394
Jonas Ferreira <44999990+JnsFerreira@users.noreply.github.com>
2021-05-09 23:21:38 -0300

    Refatorando a classe de biblioteca

--- src/library.java
+++ src/library.java
@@ -1,12 +1,19 @@
+package com.fatec.scel.model; 
 import java.util.ArrayList; 
 import java.util.List; 
 public class Biblioteca { 
- private List<Livro> livros = new ArrayList<>(); 
+ private List<Livro> livros = new ArrayList<Livro>(); 
  
- public void add(Livro livro) { 
- }
-  
+ public List<Livro> getLivros() { 
+ return livros; 
+ } 
+ public void setLivros(List<Livro> livros) { 
+ this.livros = livros; 
+ } 
+ public void save(Livro livro) { 
+ livros.add(livro); 
+ } 
  public int size() { 
-    return 1; 
+ return livros.size(); 
  } 
 }
